5a074facee6d0136a822775e54e0f5348f266304
Arian <arian.kulmer@web.de>
2024-05-22 11:56:53 +0800

    apollo: Get rid of megvii dependencies in camera.qcom.so
    This commit partially reverts https://github.com/LineageOS/android_device_xiaomi_apollon/commit/48f108d2267be5299546eb4f12461c65ed04c4aa

--- device.mk
+++ device.mk
@@ -127,10 +127,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
-    libMegviiFacepp-0.5.2 \
-    libmegface \
     libgui_vendor \
     libpiex_shim \
+    libshim_megvii \
     libstdc++_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
