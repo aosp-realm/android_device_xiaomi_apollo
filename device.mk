5f6514b567fe93f0ec6ea456c7297668bf7aba51
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-06-22 19:24:15 +0200

    apollo: Fix camera library rule location
    Change-Id: I24b30a7e187273fce2e23847debe5bff473bfe23

--- device.mk
+++ device.mk
@@ -122,7 +122,6 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    libutilscallstack.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
@@ -131,6 +130,7 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     libpiex_shim \
     libstdc++_vendor \
+    libutilscallstack.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
