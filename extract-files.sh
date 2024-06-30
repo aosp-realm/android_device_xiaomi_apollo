3aa88b2dda3228db257b98bc8c9ebfce7961552f
dogpoopy <qtqtron@gmail.com>
2024-06-30 14:54:11 +0000

    apollo: extract-files.sh: Correct append format of mifare implementation
    * https://github.com/aosp-realm/android_vendor_xiaomi_apollo/blob/7d35c1d0233a155931dc7859be8d3aa716f4bc2c/proprietary/vendor/etc/libnfc-nci.conf#L118

--- extract-files.sh
+++ extract-files.sh
@@ -28,7 +28,6 @@ function blob_fixup() {
     case "${1}" in
         vendor/etc/libnfc-nci.conf)
             cat << EOF >> "${2}"
-###############################################################################
 # Mifare Tag implementation
 # 0: General implementation
 # 1: Legacy implementation
