15ff926b257b50d6d2ecdd31c966dbc531bcb0e2
dogpoopy <qtqtron@gmail.com>
2024-07-04 18:09:54 +0800

    apollo: Switch to mini gapps

--- lineage_apollo.mk
+++ lineage_apollo.mk
@@ -11,6 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_USES_MINI_GAPPS := true
+
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
