8458e8df3ead26809e1bff5f2fbd7d62a6f1fbd5
Kyuofox <kyuofox@gmail.com>
2024-09-14 12:57:46 +0200

    apollo: Allow gettid call for atfwd
    We never know what upstream is thinking:
    W ATFWD-daemon: libminijail[4756]: failed to get path of fd 3: No such file or directory
    W ATFWD-daemon: libminijail[4756]: allowing syscall: connect
    W ATFWD-daemon: libminijail[4756]: allowing syscall: fcntl
    W ATFWD-daemon: libminijail[4756]: allowing syscall: sendto
    W ATFWD-daemon: libminijail[4756]: allowing syscall: socket
    W ATFWD-daemon: libminijail[4756]: allowing syscall: writev
    W ATFWD-daemon: libminijail[4756]: logging seccomp filter failures
    E ATFWD-daemon: libminijail[4756]: blocked syscall: gettid

--- extract-files.sh
+++ extract-files.sh
@@ -101,9 +101,14 @@ EOF
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
             ;;
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            [ "$2" = "" ] && return 0
+            echo 'gettid: 1' >> "${2}"
+            ;;
+
         *)
             return 1
-            ;;
+	    ;;
     esac
 
     return 0
